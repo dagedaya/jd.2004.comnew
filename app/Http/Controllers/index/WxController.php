2620abec6ffa5a4f7a415de9768acb648e4c690e
dagedaya <2>
2020-12-02 13:53:11 +0800

    6

--- app/Http/Controllers/index/WxController.php
+++ app/Http/Controllers/index/WxController.php
@@ -626,7 +626,7 @@ class WxController extends Controller
      * 汉字转拼音
      */
     public function chinese($data){
-        $text=$data->Content;
+        $text='嘻嘻';
         $data=file_get_contents('http://api.tianapi.com/txapi/pinyin/index?key='.env('WX_APIKEY').'&text='.$text.' ');//api接口
         $json=json_decode($data,true);//将json解析成数组
 //        if($json['code'] == 200){ //判断状态码
